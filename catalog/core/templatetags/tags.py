c0b24af2f0b72074f04715e40d1547f6355e2fbc
tgpatel <mascottejas@gmail.com>
2015-03-31 11:38:08 -0700

    refactor template tags

--- catalog/core/templatetags/tags.py
+++ catalog/core/templatetags/tags.py
@@ -1,32 +1,14 @@
-# tags.py
-# custom template tags
-# active tag taken from
-# http://gnuvince.wordpress.com/2007/09/14/a-django-template-tag-for-the-current-active-page/
-
-import os
-import re
-
 from django import template
-from django.conf import settings
+import re
 
 register = template.Library()
 
-import logging
 
-logger = logging.getLogger(__name__)
-
-
-@register.simple_tag
+@register.filter(is_safe=True)
 def active(request, pattern):
-    if request:
-        return 'active' if pattern == request.path else 'inactive'
-    else:
-        return 'inactive'
+    return 'active' if pattern == request.path else ''
 
 
-@register.simple_tag
+@register.filter(is_safe=True)
 def active_re(request, pattern):
-    if request:
-        return 'active' if re.search(pattern, request.path) else 'inactive'
-    return 'inactive'
-
+    return 'active' if re.search(pattern, request.path) else ''
