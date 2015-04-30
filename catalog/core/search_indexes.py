892d29f652c4fb22de5c3b34383d25c96ffe3038
tgpatel <mascottejas@gmail.com>
2015-04-30 13:37:53 -0700

    Refactoring search indexes by using abstract classes

--- catalog/core/search_indexes.py
+++ catalog/core/search_indexes.py
@@ -2,7 +2,11 @@ from haystack import indexes
 from .models import JournalArticle, Platform, Sponsor, Tag, Journal, ModelDocumentation
 
 
-class PublicationIndex(indexes.SearchIndex, indexes.Indexable):
+##########################################
+#  Publication query seach/filter index  #
+##########################################
+
+class PublicationIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
     pub_date = indexes.DateField(model_attr='date_published', null=True)
@@ -10,45 +14,55 @@ class PublicationIndex(indexes.SearchIndex, indexes.Indexable):
     status = indexes.CharField(model_attr='status')
     assigned_curator = indexes.CharField(model_attr='assigned_curator', null=True)
 
+    class Meta:
+        abstract = True
+
+
+class JournalArticleIndex(PublicationIndex, indexes.Indexable):
     def get_model(self):
         return JournalArticle
 
 
-class PlatformIndex(indexes.SearchIndex, indexes.Indexable):
+##########################################
+#       AutoComplete Index Fields        #
+##########################################
+
+class ValueAutoCompleteIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True)
+    value = indexes.NgramField(model_attr='value')
+
+    class Meta:
+        abstract = True
+
+
+class NameAutoCompleteIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True)
     name = indexes.NgramField(model_attr='name')
 
+    class Meta:
+        abstract = True
+
+
+class PlatformIndex(NameAutoCompleteIndex, indexes.Indexable):
     def get_model(self):
         return Platform
 
 
-class SponsorIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True)
-    name = indexes.NgramField(model_attr='name')
-
+class SponsorIndex(NameAutoCompleteIndex, indexes.Indexable):
     def get_model(self):
         return Sponsor
 
 
-class TagIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True)
-    value = indexes.NgramField(model_attr='value')
-
+class TagIndex(ValueAutoCompleteIndex, indexes.Indexable):
     def get_model(self):
         return Tag
 
 
-class JournalIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True)
-    name = indexes.NgramField(model_attr='name')
-
+class JournalIndex(NameAutoCompleteIndex, indexes.Indexable):
     def get_model(self):
         return Journal
 
 
-class ModelDocIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True)
-    value = indexes.NgramField(model_attr='value')
-
+class ModelDocIndex(ValueAutoCompleteIndex, indexes.Indexable):
     def get_model(self):
         return ModelDocumentation
