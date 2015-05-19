8c5546050838c0465f675ccfab92422b778aeb67
Allen Lee <allen.lee@asu.edu>
2015-05-19 16:53:44 -0700

    add default django password reset urls and views
    should update to maintain consistent styling eventually

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/password_change/$', auth_views.password_change, name='password_change'),
+    url(r'^accounts/password_change/done$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^accounts/password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^accounts/password_reset_done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'', include('catalog.core.urls', namespace='core')),
 ]
