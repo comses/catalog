05ec1dd27e3c01040ee2f302de32d1d95bc840fb
Allen Lee <allen.lee@asu.edu>
2016-04-19 13:39:13 -0700

    add django 1.9 compliance

--- catalog/core/models.py
+++ catalog/core/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.contrib.sites.models import RequestSite
+from django.contrib.sites.requests import RequestSite
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
 from django.db import models
