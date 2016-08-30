5edec3770e68a42b20c40da79f83c5decd6d2b7c
Allen Lee <allen.lee@asu.edu>
2016-08-30 16:33:59 -0700

    fix wait-for-it and add uwsgi touch-reload

--- deploy/docker/prod.sh
+++ deploy/docker/prod.sh
@@ -4,5 +4,5 @@
 cd /code
 python manage.py collectstatic --noinput --clear
 chmod a+x /etc/periodic/daily/*
-/code/deploy/docker/wait-for-it.sh -- python manage.py rebuild_index --noinput
+/code/deploy/docker/wait-for-it.sh solr:8983 -- python manage.py rebuild_index --noinput
 uwsgi --ini /code/deploy/uwsgi/catalog.ini
