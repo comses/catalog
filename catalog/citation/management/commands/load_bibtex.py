6ef92a473622a72a02deec6a7ac3a814e90c3653
Allen Lee <allen.lee@asu.edu>
2016-09-28 14:05:37 -0700

    use pathlib to scan and load data directories
    - use -d <directory-of-bibtex-files> or -f <single-bibtex-file>
    - removed explicit output files for storing invalid / duplicate entries,
      autogenerating them instead

--- catalog/citation/management/commands/load_bibtex.py
+++ catalog/citation/management/commands/load_bibtex.py
@@ -2,10 +2,9 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 from ... import bibtex as bibtex_api
-from ...crossref import doi_lookup as crossref_doi_api, author_year_lookup as crossref_author_year_api
 
 import logging
-import os
+import pathlib
 import pickle
 
 logger = logging.getLogger(__name__)
@@ -16,31 +15,35 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('-f', '--filename',
-                            required=True,
                             help='BibTeX file to load into the DB')
-        parser.add_argument('-o', '--output',
-                            required=True,
-                            help='File to log errors to')
+        parser.add_argument('-d', '--directory',
+                            help='Directory of data files to load into the DB')
         parser.add_argument('-u', '--username',
                             required=True,
                             help='username to load the data into the db as')
 
-    @staticmethod
-    def write_publication_errors(file_name, publication_errors):
-        with open(file_name, "w") as f:
-            pickle.dump(publication_errors, f, protocol=pickle.HIGHEST_PROTOCOL)
-
     def handle(self, *args, **options):
-        filename = options['filename']
-        output = options['output']
+        filename = options.get('filename')
+        directory = options.get('directory')
+        if not any([filename, directory]):
+            raise ValueError("no input filename or directory given")
         username = options['username']
         user = User.objects.get(username=username)
-        if not os.path.exists(filename):
-            ValueError("File %s does not exist" % filename)
-        if os.path.exists(output):
-            os.unlink(output)
-        errors_and_duplicates = bibtex_api.process_entries(filename, user)
-        with open(output, 'wb') as f:
-            print("Saving Duplicate Errors to File %s" % output)
+        file_paths = []
+        if directory:
+            file_paths = [p for p in pathlib.Path(directory).iterdir() if p.is_file()]
+        elif filename:
+            file_paths = [pathlib.path(filename)]
+        for p in file_paths:
+            self.process_filepath(p, user)
+
+    def process_filepath(self, path, user):
+        output = path.parent.joinpath("invalid-{0}.p".format(path.name))
+        if output.exists():
+            logger.debug("deleting old output file %s", output)
+            output.unlink()
+        errors_and_duplicates = bibtex_api.process_entries(path.absolute().as_posix(), user)
+        with output.open('wb') as f:
+            logger.debug("Pickling duplicate errors to %s", output)
             pickle.dump(errors_and_duplicates, f)
-        print("Done loading")
+        logger.debug("Done loading")
