8d39058ef4c699b172e25fcbd953a2539d63a170
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-11-20 12:37:11 -0700

    made bokeh configuration dependent on DOMAIN_NAME

--- deploy/docker/bokeh.sh
+++ deploy/docker/bokeh.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o pipefail
+set -o nounset
+
+DOMAIN_NAME=${DOMAIN_NAME:-localhost}
+
 cd /code/visualization/bokeh_example
-exec bokeh serve --address 0.0.0.0 --allow-websocket-origin=localhost visualization
+./wait-for-it.sh elasticsearch:9200 -- echo "ElasticSearch is ready. Starting bokeh"
+exec bokeh serve --address 0.0.0.0 --allow-websocket-origin=${DOMAIN_NAME} visualization
--- deploy/docker/prod.sh
+++ deploy/docker/prod.sh
@@ -5,6 +5,9 @@ cd /code
 python3 manage.py collectstatic --noinput --clear
 chmod a+x /etc/cron.daily/*
 chmod a+x /etc/cron.monthly/*
-# /code/deploy/docker/wait-for-it.sh solr:8983 -- python3 manage.py rebuild_index --noinput
-/code/deploy/docker/wait-for-it.sh elasticsearch:9200
+/code/deploy/docker/wait-for-it.sh solr:8983 -- echo "Solr is ready."
+/code/deploy/docker/wait-for-it.sh elasticsearch:9200 -- echo "ElasticSearch is ready."
+#echo "Indexing elasticsearch and solr"
+#python3 manage.py rebuild_index --noinput
+#echo "Starting UWSGI"
 exec uwsgi --ini /code/deploy/uwsgi/catalog.ini
