0176316f37c0f298c895c5121b09d12ef534bec9
Calvin Pritchard <pritchard.calvin@gmail.com>
2018-12-19 12:44:54 -0700

    Updated urls to use path function instead of regex matching with url function

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from cas import views as cas_views
@@ -8,16 +8,16 @@ from django.urls import path
 from catalog.core.views import LoginView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
-    url(r'^cas/asu/login/$', cas_views.login, name='cas_login'),
-    url(r'^accounts/logout/$', cas_views.logout, name='logout'),
-    url(r'^accounts/password_change/$', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    url(r'^accounts/password_change/done$', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    url(r'^accounts/password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    url(r'^accounts/password_reset_done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    url(r'^', include('citation.urls')),
-    url(r'', include('catalog.core.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('cas/asu/login/', cas_views.login, name='cas_login'),
+    path('accounts/logout/', cas_views.logout, name='logout'),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('', include('citation.urls')),
+    path('', include('catalog.core.urls')),
 ]
 
 if settings.DEBUG:
